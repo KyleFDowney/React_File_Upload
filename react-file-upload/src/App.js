7eccd19fe3002a8555c0fad59795517a1233aba7
capntechbeard <capntechbeard@gmail.com>
2019-07-09 17:12:10 -0500

    axios

--- react-file-upload/src/App.js
+++ react-file-upload/src/App.js
@@ -1,15 +1,32 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import axios from 'axios';
 
-onChangeHandler = event => {
-  console.log(event.target.files[0]);
-};
+constructor(props) {
+  super(props);
+    this.state = {
+      selectedFile: null
+    }
+ 
+}
+
+onChangeHandler=event=>{
+  this.setState({
+    selectedFile: event.target.files[0],
+    loaded: 0,
+  })
+}
+onClickHandler = () => {
+  const data = new FormData() 
+  data.append('file', this.state.selectedFile)
+}
+
 
 function App() {
   return (
     <div className="App">
       <input type="file" name="file" onChange={this.onChangeHandler} />
+      <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button> 
     </div>
   );
 }
